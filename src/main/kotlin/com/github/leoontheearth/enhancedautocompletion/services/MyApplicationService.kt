69f5b7541a4d4e4eb3e2402809a78f7a4f7b8de1
GitHub Action <action@github.com>
2021-03-02 17:44:02 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/leoontheearth/enhancedautocompletion/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.leoontheearth.enhancedautocompletion.services
+
+import com.github.leoontheearth.enhancedautocompletion.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
